f95779ae035deace990eb5066884f26b7a9cf7f2
10sr <8.slashes@gmail.com>
2020-10-07 07:29:01 +0900

    TMP debug

--- ert-tests/editorconfig.el
+++ ert-tests/editorconfig.el
@@ -43,6 +43,7 @@
 
   (with-visit-file (concat editorconfig-ert-dir
                            "3_space.txt")
+    (message "%S" editorconfig-mode)
     (should (eq tab-width 3))
     (should (eq indent-tabs-mode nil)))
 
